9bc7308868f1efab25d38aa366dbf4cabab58484
Nathan Bailey <dev@dnbailey.org>
2017-11-09 10:07:51 -0500

    Fix directory structure

--- render.js
+++ render.js
@@ -1,21 +1,25 @@
-const pug = require('pug');
 const express = require('express');
+const path = require('path');
+const router = express.Router();
 const app = express();
 
-const compiledFunction = pug.compileFile('template.pug');
-const compiledFunction1 = pug.compileFile('template2.pug')
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', function(req, res) {
-  res.send(compiledFunction({
-    user: '_r3c0n_'
-  }));
+  res.render('index', { user: '_r3c0n_' })
 });
+
 app.get('/announcements', function(req, res) {
-  res.send(compiledFunction1({
+  res.render('announcements',{
     announcement: 'hhhh',
     datebeg: '8-1-17',
     dateend: '8-6-17',
-  }));
+  });
 });
-app.listen('1000', function() {
+
+app.listen('3000', function() {
   console.log('[*] Listening on port 3000');
 })
